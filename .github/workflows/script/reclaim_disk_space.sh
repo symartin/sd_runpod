3e67baf8e207c90786e4875716f884cca6fa94c1
Sylvain Martin <31699829+symartin@users.noreply.github.com>
2023-01-17 01:29:26 +0100

    update large packages

--- .github/workflows/script/reclaim_disk_space.sh
+++ .github/workflows/script/reclaim_disk_space.sh
@@ -4,32 +4,34 @@ set -eu
 
 echo "Memory and swap:"
 free
-echo
+echo "---"
 swapon --show
-echo
-        
+
+echo "---"  
 echo "Disk space report before modification"
 df -h
 
+echo "---"
 echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 
-
+echo "---"
 echo "remove 4GiB of images"
 sudo systemd-run docker system prune --force --all --volumes
 df -h
 
+echo "---"
 echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-sudo apt-get remove -y firefox
+sudo apt-get remove -y '^dotnet.*'
+sudo apt-get remove -y '^mysql.*'
+sudo apt-get remove -y '^linux-azure-headers.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell microsoft-edge-stable 
+sudo apt-get remove -y firefox aspnetcore-runtime-6.0 humanity-icon-theme
 sudo apt-get autoremove -y
 sudo apt-get clean
 df -h
 
+echo "---"
 echo "remove large folder"
 sudo systemd-run rm -rf \
   "$AGENT_TOOLSDIRECTORY" \
@@ -42,6 +44,7 @@ sudo systemd-run rm -rf \
   /usr/share/swift \
   /usr/local/lib/android \
   /usr/local/share/boost
-  
+
+echo "---"
 echo "Free space after reclaim"  
 df -h
